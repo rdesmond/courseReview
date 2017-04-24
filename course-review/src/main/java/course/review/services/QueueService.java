d93625e594b0f26980f9dea7674c9d6fead1ed6a
Ryan Desmond <radesmond@gmail.com>
2017-04-24 14:14:01 +0800

    Update QueueService.java

--- course-review/src/main/java/course/review/services/QueueService.java
+++ course-review/src/main/java/course/review/services/QueueService.java
@@ -2,6 +2,10 @@ package course.review.services;
 
 //This class demonstrates a queue
 public class QueueService {
+    
+    // RYAN: if anyone were to use this Queue they'd have complete access to the underlying datastructure (array)
+    // as well as the putloc and getloc indices = not good. This would cause serious problems.
+    // This queue is also not generic, so you can only use it with an int array of a predeclared size = not good.
     int q[];
     int putlocation;
     int getlocation;
